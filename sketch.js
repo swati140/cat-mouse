88e90bf70d7fa6dfb7220b310addfdee3a743e33
swati140 <74068597+swati140@users.noreply.github.com>
2021-01-26 11:02:33 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,13 +4,13 @@ var tom, tomImg1,tomImg2;
 var jerry, jerryImg1,jerryImg2;
 
 function preload() {
-    bg = loadImage("images/garden.png");
-    tomImg1= loadAnimation("images/tomOne.png");
-    tomImg2=loadAnimation("images/tomTwo.png","images/tomThree.png");
-    tomImg3= loadAnimation("images/tomFour.png");
-    jerryImg1=loadAnimation("images/jerryOne.png");
-    jerryImg2= loadAnimation("images/jerryTwo.png","images/jerryThree.png");
-    jerryImg3=loadAnimation("images/jerryFour.png");
+    bg = loadImage("garden.png");
+    tomImg1= loadAnimation("tomOne.png");
+    tomImg2=loadAnimation("tomTwo.png","tomThree.png");
+    tomImg3= loadAnimation("tomFour.png");
+    jerryImg1=loadAnimation("jerryOne.png");
+    jerryImg2= loadAnimation("jerryTwo.png","jerryThree.png");
+    jerryImg3=loadAnimation("jerryFour.png");
 
 }
 
